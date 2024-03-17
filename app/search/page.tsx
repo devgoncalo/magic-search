257a203e6e3fc9b9dc9e9831e1031af929f5ad7f
devgoncalo <goncalopintoneto@gmail.com>
2024-03-17 16:27:57 +0000

    feat: Querying Logic

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,3 +1,7 @@
+import { db } from "@/db";
+import { productsTable } from "@/db/schema";
+import { sql } from "drizzle-orm";
+
 import { redirect } from "next/navigation";
 
 interface PageProps {
@@ -6,16 +10,27 @@ interface PageProps {
   };
 }
 
-const Page = ({ searchParams }: PageProps) => {
+const Page = async ({ searchParams }: PageProps) => {
   const query = searchParams.query;
 
-  if (Array.isArray(query)) {
+  if (Array.isArray(query) || !query) {
     return redirect("/");
   }
 
-  // TODO: Querying Logic
+  let products = await db
+    .select()
+    .from(productsTable)
+    .where(
+      sql`to_tsvector('simple', lower(${productsTable.name} || ' ' || ${
+        productsTable.description
+      })) @@ to_tsquery('simple', lower(${query
+        .trim()
+        .split(' ')
+        .join(' & ')}))`
+    )
+    .limit(3)
 
-  return <div>Search</div>;
+  return <pre>{JSON.stringify(products)}</pre>;
 };
 
 export default Page;
--- db/index.ts
+++ db/index.ts
@@ -1,6 +1,6 @@
 import { env } from '@/env';
 
-import { drizzle } from 'drizzle-orm/node-postgres'
+import { drizzle } from 'drizzle-orm/postgres-js'
 import postgres from 'postgres'
 
 const connectionString = env.DATABASE_URL
